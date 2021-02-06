b66479e392291e0d286c2d5468a72c61af3cffe7
Oscar Parada <op66954@uga.edu>
2021-02-06 13:34:40 -0500

    added basic structure

--- app/src/main/java/com/motivatedmind/samaritan/Profile.java
+++ app/src/main/java/com/motivatedmind/samaritan/Profile.java
@@ -1,4 +1,39 @@
 package com.motivatedmind.samaritan;
 
+import android.media.Image;
+
 public class Profile {
+    String name;
+    String age;
+    String occupation; //job, education etc.
+    String biography;
+    Image profilePicture;
+
+    public Profile(String name, String age, String occupation, String biography, Image profilePicture) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+        this.biography = biography;
+        this.profilePicture = profilePicture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Image getProfilePicture() {
+        return profilePicture;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
 }
